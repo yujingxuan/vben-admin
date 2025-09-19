b657789a21239783e0de6d0742a84157ad87274a
yujingxuan <yu-jingxuan@qq.com>
2025-09-19 19:11:21 +0800

    解决冲突

---
+++ packages/pro/src/Test/index.ts
@@ -0,0 +1,3 @@
+export { default as Test } from './index.vue';
+
+export { default as Table } from './table.vue';
---
+++ packages/pro/src/Test/index.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts">
+import TestComponent from './test';
+</script>
+
+<template>
+  <TestComponent />
+</template>
